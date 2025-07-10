13b5ecd325c445aa905c0615520413a3ce431e06
HectorGuerra004 <hectorsantiagoguerra004@gmail.com>
2025-07-10 00:31:01 -0400

    iconos en local

--- FindMe/src/utils/api.js
+++ FindMe/src/utils/api.js
@@ -1,6 +1,6 @@
 import axios from 'axios';
 const api = axios.create({
-  baseURL: "http://192.168.1.107:8000/api/",
+  baseURL: "http://192.168.1.105:8000/api/",
   // baseURL: "http://localhost:8000/api/",
   withCredentials: true,
   headers: {
